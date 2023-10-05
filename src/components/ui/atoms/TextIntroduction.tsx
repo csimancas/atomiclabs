8286072f3cf6fa7d575b1561139b225a528f4178
csimancas <csimancas@csimancass-MacBook-Pro.local>
2023-10-04 19:32:42 -0700

    visual change text

--- src/components/ui/atoms/TextIntroduction.tsx
+++ src/components/ui/atoms/TextIntroduction.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import {colors} from '../../../utils/colors';
 
 const TextIntroduction = () => {
   return (
-    <Text style={styles.container}>
-      <Text>
-        <Text style={styles.title}>Desarrolla todo{'\n'}</Text>
-        <Text style={styles.orangeOne}>tu PONTENCIAL{'\n'}</Text> dentro del
-        equipo
-        {'\n'}
-        <Text style={styles.orangeTwo}>
-          ATOMIC<Text style={{color: colors.white}}>LABS</Text>
+    <View style={styles.container}>
+      <Text style={styles.containerText}>
+        <Text>
+          <Text style={styles.title}>Desarrolla todo{'\n'}</Text>
+          <Text style={styles.orangeOne}>tu PONTENCIAL{'\n'}</Text> dentro del
+          equipo
+          {'\n'}
+          <Text style={styles.orangeTwo}>
+            ATOMIC<Text style={{color: colors.white}}>LABS</Text>
+          </Text>
         </Text>
       </Text>
-    </Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerText: {
     marginTop: 20,
     paddingHorizontal: 15,
     textAlign: 'justify',
     fontSize: 38,
     fontWeight: 'bold',
     color: colors.white,
-    //letterSpacing: ,
   },
   title: {
     letterSpacing: 4,
